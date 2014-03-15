8835b39dc2a82f8113425c99906a64893ee5cbd6
caskethopper <mcce8162@email.cpcc.edu>
2014-03-15 14:35:48 -0400

    Fixing output
    Had some issues getting properties and putting the info into a parsable
    format. Should be fixed now.

--- ADU-LookupByName.ps1
+++ ADU-LookupByName.ps1
@@ -1,7 +1,27 @@
-$userlist = import-csv c:\temp\ulist-import.csv
+$userlist = get-content c:\temp\Import-CSV.txt
+
 
 foreach ($user in $userlist){
+  $filter = "displayname -eq ""$user"""
+  $userdetail = get-aduser -filter $filter -properties * -searchbase "ou=allusers,dc=cpcc,dc=edu" -searchscope 2
+
+  $dname = $userdetail.DisplayName
+  $dname = $dname -replace "`t|`n|`r",""
+
+  $dept = $userdetail.Department
+  $dept = $dept -replace "`t|`n|`r",""
+
+  $formatstring = "$dname,$dept"
+  $formatstring | Add-Content 'c:\temp\adoutput.txt'
   
-  get-aduser -filter {displayName -like "$user"} -properties department, displayName | select displayName, department | export-csv "ADOut.out" 
+
+### Added to an array, but I think I'm just going to write straight to the file
+#  $myoutput += $formatstring
+
+
+###
+### Working output, but gd newlines
+###
+#  write-output $userdetail.DisplayName "," $userdetail.Department
 
 }
